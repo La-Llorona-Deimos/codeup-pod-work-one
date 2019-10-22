db1cae28d167d5fccdb94e7b1c919361361e256b
Karli Steeves <steeves.karli@gmail.com>
2019-10-22 16:31:59 -0500

    second exercise done.

--- karli_js.js
+++ karli_js.js
@@ -8,3 +8,34 @@ function alphabet_order(str)
 console.log(alphabet_order("little puppy dog"));
 
 //I got this from W3Resources
+
+///Mr. despair wants to jump off Dutch act, So he came to the top of a building.
+//
+// Scientific research shows that a man jumped from the top of the roof,
+// when the floor more than 6, the person will often die in an instant; When the floor is less than or equal to 6,
+// the person will not immediately die, he would scream. (without proof).
+
+//Mr. despair jumped from the 2 floor, the voice is "Aa~"
+//He fell on the ground, the voice is "Pa!"
+//He did not die immediately, and the final voice was "Aa!"
+
+//if floor<=1, Mr. despair is safe, return ""
+function willManDie(floor){
+    if(floor === 1){
+        return "No harm done."
+    } else if (floor <= 6){
+        return "AHHH! MY LEGS!!"
+    } else {
+        return "Instant death."
+    }
+}
+
+console.log(willManDie(4));
+
+
+//I got this problem from CodeWars
+
+
+
+//Write a function, getOlder() that takes in array of dog objects and increases the value of each object's age property by 1.
+
