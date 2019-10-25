29a53a3a354ecff0a7992dfb685b9eea3d2b48b0
Roger Mayer <rmayer1984@gmail.com>
2019-10-25 16:05:56 -0500

    roger's files added

---
+++ js/roger-dice-js.js
@@ -0,0 +1,87 @@
+(function(){
+    "use strict";
+    //craps
+//     Below is a list of the various bets you can make at craps. Pass Line Bet -
+//     You win if the first roll is a natural (7, 11) and lose if it is craps (2, 3, 12).
+//     If a point is rolled (4, 5, 6, 8, 9, 10) it must be repeated before a 7 is thrown
+//     in order to win. If 7 is rolled before the point you lose.
+
+//     var play = true;
+    var rollAgain = false;
+    var bank = 5;
+    var i = 0;
+    var play = document.getElementById('playCraps');
+    displayBank();
+
+// if (typeof play === "number" ){
+
+    var roll = function (x, y) {
+        if (bank === 0){
+            alert("you are out of money");
+        } else {
+            var bet = parseFloat(prompt("you have $" + bank + ", how much do you want to bet?"));
+            x = Math.floor(Math.random() * (6) + 1);
+            y = Math.floor(Math.random() * (6) + 1);
+            var total = x + y;
+            console.log("you rolled a " + x + " and " + y + " for a total of " + total);
+            alert("you rolled a " + x + " and " + y + " for a total of " + total);
+
+            if (total === 7 || total === 11) {
+                alert("a first roll of " + total + " means you win!");
+                bank += bet;
+
+                alert("you have $" + bank);
+            } else if (total === 2 || total === 3 || total === 12) {
+                console.log("a first roll of " + total + " means you lose!");
+                alert("a first roll of " + total + " means you lose!");
+                bank -= bet;
+                alert("you have $" + bank);
+            } else {
+                console.log("a first roll of " + total + " means roll again");
+                alert("a first roll of " + total + " means roll again");
+                secondRoll();
+            }
+
+            function secondRoll() {
+                var rollAgain = true;
+                while (rollAgain === true) {
+                    var xx = Math.floor(Math.random() * (6) + 1);
+                    var yy = Math.floor(Math.random() * (6) + 1);
+                    var totalTwo = xx + xx;
+                    if (total === totalTwo) {
+                        alert("the first roll of " + total + " and this roll of " + totalTwo + " match. This means you win!");
+                        bank += bet;
+                        rollAgain = false;
+                        alert("you have $" + bank);
+                    } else if (totalTwo === 7 || totalTwo === 2 || totalTwo === 11) {
+                        console.log("you rolled " + totalTwo + ". This means you lose!");
+                        alert("you rolled " + totalTwo + ". This means you lose!");
+                        bank -= bet;
+                        rollAgain = false;
+                        alert("you have $" + bank);
+                    } else {
+                        console.log("you rolled " + totalTwo + ". This means roll again");
+                        alert("you rolled " + totalTwo + ". This means roll again");
+                        rollAgain = true;
+                    }
+
+                }
+
+            }
+            displayBank();
+        }
+    }
+// } else {
+//     alert("Thanks for stopping by");
+// }
+
+    function displayBank(){
+        document.getElementById("bankAmount").innerHTML = "Bank $" + bank ;
+        console.log(bank);
+    }
+    document.getElementById("playCraps").addEventListener("click", roll)
+
+
+
+
+})();
---
+++ js/roger-js.js
@@ -0,0 +1,135 @@
+(function () {
+    "use strict";
+    // https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
+    // Implement a function likes :: [String] -> String, which must take in input array,
+    // containing the names of people who like an item.
+    // It must return the display text as shown in the examples:
+    // 0 like = no one likes this
+    // 1 likes= ...likes this
+    // 2 likes= ... and ... likes this
+    // 3 likes= ..., ... and ... like this
+    // 4+ likes= ..., ... and # others like this
+
+    function likes(names) {
+        if (names.length === 0) {
+            return "no one likes this";
+        } else if (names.length === 1) {
+            return names[0] + " likes this";
+        } else if (names.length === 2) {
+            return names[0] + " and " + names[1] + " like this";
+        } else if (names.length === 3) {
+            return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+        } else if (names.length > 3) {
+            return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+        }
+    }
+
+    console.log(likes([]), 'no one likes this');
+    console.log(likes(['Peter']), 'Peter likes this');
+    console.log(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+    console.log(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+    console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
+
+
+
+
+
+
+//coffee objects
+
+    var coffees = [
+        {
+            name: "Inza #2",
+            roaster: "Flat Track",
+            country: "Columbia",
+            region: "South America",
+            altitude: 1800,
+            price: 18.00,
+            process: "washed",
+            notes: ["plum", "caramel", "black cherry"]
+        },
+        {
+            name: "Chirinos",
+            roaster: "Wild Gift",
+            country: "Peru",
+            region: "South America",
+            altitude: 1700,
+            price: 16.00,
+            process: "washed",
+            notes: ["plum", "Sugar", "apple"]
+        },
+        {
+            name: "Finca De Dios",
+            roaster: "Madcap",
+            country: "Guatemala",
+            region: "Central America",
+            altitude: 1750,
+            price: 18.00,
+            process: "washed",
+            notes: ["lime", "peach", "cardamom"]
+        },
+        {
+            name: "Chelba",
+            roaster: "Cat and Cloud",
+            country: "Ethiopia",
+            region: "Africa",
+            altitude: 1900,
+            price: 17.50,
+            process: "Natural",
+            notes: ["blueberry", "chocolate", "port sweetness"]
+        },
+        {
+            name: "Worka Sakaro",
+            roaster: "Heart",
+            country: "Ethiopia",
+            region: "Africa",
+            altitude: 2100,
+            price: 19.00,
+            process: "washed",
+            notes: ["mango", "currant", "brown sugar"]
+        },
+    ];
+
+
+    function showCoffeeInfo(coffee, i) {
+        var output = "";
+        output += "#" + (i + 1) + "\n";
+        output += "Name: " + coffee.name + "\n";
+        output += "Roaster: " + coffee.roaster + "\n";
+        output += "Country: " + coffee.country + "\n";
+        output += "Region: " + coffee.region + "\n";
+        output += "Altitude: " + coffee.altitude + "\n";
+        output += "Price: " + coffee.price + "\n";
+        output += "Process: " + coffee.process + "\n";
+        output += "Tasting Notes: " + coffee.notes[0]  + ", " + coffee.notes[1]+ ", " + coffee.notes[2]  + "\n";
+        output += "---";
+        console.log(output);
+    }
+    coffees.forEach(showCoffeeInfo);
+
+    coffees.forEach(function (coffee) {
+    console.log(coffee.name + " cost $" + coffee.price)
+});
+
+    var input = document.getElementById("initial");
+    var submit = document.getElementById("ok");
+    var value = "";
+    var splitted = "";
+    var output = document.getElementById("output");
+
+    submit.addEventListener("click", function() {
+        value = input.value;
+        splitted = value.split("");
+        var lSplitted = splitted.length;
+
+        for (var i = 0; i < lSplitted; i += 2) {
+            splitted[i] = splitted[i].toUpperCase();
+        }
+
+        for (var i = 1; i < lSplitted; i += 3) {
+            splitted[i] = splitted[i].toLowerCase();
+        }
+
+        output.innerHTML = splitted.join('');
+    });
+})();
