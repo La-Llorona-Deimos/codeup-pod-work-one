da6d7cfa5cb104e1b1bcbc0c93a4e2944efea237
Roger Mayer <rmayer1984@gmail.com>
2019-11-08 08:28:11 -0600

    completed JS ex

--- js/roger-js.js
+++ js/roger-js.js
@@ -134,7 +134,8 @@
 //     });
 
 
-//pyramid
+//pyramid of stars
+
 var z = parseInt(prompt("how long would you like your triangle?"));
 var x,y,chr;
 function pyramid(z) {
@@ -151,4 +152,72 @@ for(x=1; x <=z; x++)
 }
 
     console.log(pyramid(z));
+
+
+//write for loop to indicate if book has been read or not
+
+var library = [
+    {
+        title: 'Bill Gates',
+        author: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        title: 'Steve Jobs',
+        author: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        author: 'Suzanne Collins',
+        readingStatus: false
+    }];
+
+for (var i = 0; i < library.length; i++) {
+    var book = "'" + library[i].title + "'" + ' by ' + library[i].author + ".";
+    if (library[i].readingStatus) {
+        console.log("Already read " + book);
+    } else {
+        console.log("You still need to read " + book);
+    }
+
+}
+    //There are two arrays with individual values, write a JavaScript program to compute the sum of each individual index value from the given arrays.
+    function Arrays_sum(array1, array2)
+    {
+        var result = [];
+        var ctr = 0;
+        var x=0;
+
+        if (array1.length === 0)
+            return "array1 is empty";
+        if (array2.length === 0)
+            return "array2 is empty";
+
+        while (ctr < array1.length && ctr < array2.length)
+        {
+            result.push(array1[ctr] + array2[ctr]);
+            ctr++;
+        }
+
+        if (ctr === array1.length)
+        {
+            for (x = ctr; x < array2.length; x++)   {
+                result.push(array2[x]);
+            }
+        }
+        else
+        {
+            for (x = ctr; x < array1.length; x++)
+            {
+                result.push(array1[x]);
+            }
+        }
+        return result;
+    }
+
+    console.log(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13]));
+
+
+
 })();
