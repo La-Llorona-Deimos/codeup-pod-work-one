b5a095f0d0a8557a7f3cda8144b3249ed57416f1
Karli Steeves <steeves.karli@gmail.com>
2019-11-07 20:21:26 -0600

    2/3 exercises done

--- js/podexercises.js
+++ js/podexercises.js
@@ -37,3 +37,34 @@ function changeEnough(change, amountDue) {
 console.log(changeEnough([0, 0, 20, 5], 0.75));
 
 
+
+//next week exercises
+
+//Create a function that takes two numbers and a mathematical operator + - / *
+// and will perform a calculation with the given numbers.
+
+function calculator(num1, operator, num2) {
+    if(operator == '+') return num1+num2;
+    if(operator == '-') return num1-num2;
+    if(operator == '*') return num1*num2;
+    if(operator == '/' && num2 != 0) return num1/num2;
+    else return "Can't divide by 0!";
+}
+
+calculator().log
+
+//Create a function that takes in an array (slot machine outcome) and returns true if all elements in the array are identical,
+// and false otherwise. The array will contain 4 elements.
+
+function testJackpot(result) {
+    for(var i=0; i<result.length-1; i++) {
+        if(result[i] !== result[i+1] ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(testJackpot(["blue, blue, blue, blue"]));
+
+//
